c942d2a21d4721775c526db8964d826703b22e98
Simeon Revales <simeonrevales@gmail.com>
2020-04-05 12:27:20 -0400

    caesar_cipher code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,6 +1,44 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
-get '/' do
-    'Put this in your pipe & smoke it!'
+def caesar_cipher(str, num)
+
+    return "" if !str
+    num = num.to_i
+    alphabet = ('a'..'z').to_a
+    arr = str.split('')
+    ans = []
+    i = 0
+  
+    arr.each do |letter|
+  
+     case_changed = false
+      if letter.upcase == letter  
+          letter = letter.downcase
+          case_changed = true
+      end
+      
+      if alphabet.include? letter 
+        temp = alphabet[(alphabet.index(letter) + num) % alphabet.size] #use modulo to wrap array. If there is remainder, then array will wrap the beginning
+        if case_changed
+          temp = temp.upcase
+        end
+        ans[i]=temp
+      else 
+        ans[i] = letter
+      end
+      i += 1 
+    end 
+    str = ans.join('')
+    str
   end
+
+get '/' do
+    word = params["word"]
+    shift = params["shift"]
+
+    result = caesar_cipher(word, shift)
+    erb :index,
+    :locals => {:word => word, :shift => shift, :result => result}
+    
+end
